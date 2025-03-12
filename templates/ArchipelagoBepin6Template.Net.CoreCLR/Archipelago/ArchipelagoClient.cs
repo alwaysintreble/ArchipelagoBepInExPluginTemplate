4caeccab2f611ad6b67f9baa6333d8926de8b47c
alwaysintreble <mmmcheese158@gmail.com>
2025-03-11 21:20:31 -0500

    add bepin6.core template

---
+++ templates/ArchipelagoBepin6Template.Net.CoreCLR/Archipelago/ArchipelagoClient.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Archipelago.MultiClient.Net;
+using Archipelago.MultiClient.Net.BounceFeatures.DeathLink;
+using Archipelago.MultiClient.Net.Enums;
+using Archipelago.MultiClient.Net.Helpers;
+using Archipelago.MultiClient.Net.Packets;
+using ArchipelagoBepin6Template.Net.CoreCLR.Utils;
+
+namespace ArchipelagoBepin6Template.Net.CoreCLR.Archipelago;
+
+public class ArchipelagoClient
+{
+    public const string APVersion = "0.5.0";
+    private const string Game = "My Game";
+
+    public static bool Authenticated;
+    private bool attemptingConnection;
+
+    public static ArchipelagoData ServerData = new();
+    private DeathLinkHandler DeathLinkHandler;
+    private ArchipelagoSession session;
+
+    /// <summary>
+    /// call to connect to an Archipelago session. Connection info should already be set up on ServerData
+    /// </summary>
+    /// <returns></returns>
+    public void Connect()
+    {
+        if (Authenticated || attemptingConnection) return;
+
+        try
+        {
+            session = ArchipelagoSessionFactory.CreateSession(ServerData.Uri);
+            SetupSession();
+        }
+        catch (Exception e)
+        {
+            Plugin.BepinLogger.LogError(e);
+        }
+
+        TryConnect();
+    }
+
+    /// <summary>
+    /// add handlers for Archipelago events
+    /// </summary>
+    private void SetupSession()
+    {
+        session.MessageLog.OnMessageReceived += message => ArchipelagoConsole.LogMessage(message.ToString());
+        session.Items.ItemReceived += OnItemReceived;
+        session.Socket.ErrorReceived += OnSessionErrorReceived;
+        session.Socket.SocketClosed += OnSessionSocketClosed;
+    }
+
+    /// <summary>
+    /// attempt to connect to the server with our connection info
+    /// </summary>
+    private void TryConnect()
+    {
+        try
+        {
+            // it's safe to thread this function call but unity notoriously hates threading so do not use excessively
+            ThreadPool.QueueUserWorkItem(
+                _ => HandleConnectResult(
+                    session.TryConnectAndLogin(
+                        Game,
+                        ServerData.SlotName,
+                        ItemsHandlingFlags.NoItems, // TODO make sure to change this line
+                        new Version(APVersion),
+                        password: ServerData.Password,
+                        requestSlotData: false // ServerData.NeedSlotData
+                    )));
+        }
+        catch (Exception e)
+        {
+            Plugin.BepinLogger.LogError(e);
+            HandleConnectResult(new LoginFailure(e.ToString()));
+            attemptingConnection = false;
+        }
+    }
+
+    /// <summary>
+    /// handle the connection result and do things
+    /// </summary>
+    /// <param name="result"></param>
+    private void HandleConnectResult(LoginResult result)
+    {
+        string outText;
+        if (result.Successful)
+        {
+            var success = (LoginSuccessful)result;
+
+            ServerData.SetupSession(success.SlotData, session.RoomState.Seed);
+            Authenticated = true;
+
+            DeathLinkHandler = new(session.CreateDeathLinkService(), ServerData.SlotName);
+#if NET35
+            session.Locations.CompleteLocationChecksAsync(null, ServerData.CheckedLocations.ToArray());
+#else
+            session.Locations.CompleteLocationChecksAsync(ServerData.CheckedLocations.ToArray());
+#endif
+            outText = $"Successfully connected to {ServerData.Uri} as {ServerData.SlotName}!";
+
+            ArchipelagoConsole.LogMessage(outText);
+        }
+        else
+        {
+            var failure = (LoginFailure)result;
+            outText = $"Failed to connect to {ServerData.Uri} as {ServerData.SlotName}.";
+            outText = failure.Errors.Aggregate(outText, (current, error) => current + $"\n    {error}");
+
+            Plugin.BepinLogger.LogError(outText);
+
+            Authenticated = false;
+            Disconnect();
+        }
+
+        ArchipelagoConsole.LogMessage(outText);
+        attemptingConnection = false;
+    }
+
+    /// <summary>
+    /// something went wrong, or we need to properly disconnect from the server. cleanup and re null our session
+    /// </summary>
+    private void Disconnect()
+    {
+        Plugin.BepinLogger.LogDebug("disconnecting from server...");
+#if NET35
+        session?.Socket.Disconnect();
+#else
+        session?.Socket.DisconnectAsync();
+#endif
+        session = null;
+        Authenticated = false;
+    }
+
+    public void SendMessage(string message)
+    {
+        session.Socket.SendPacketAsync(new SayPacket { Text = message });
+    }
+
+    /// <summary>
+    /// we received an item so reward it here
+    /// </summary>
+    /// <param name="helper">item helper which we can grab our item from</param>
+    private void OnItemReceived(ReceivedItemsHelper helper)
+    {
+        var receivedItem = helper.DequeueItem();
+
+        if (helper.Index <= ServerData.Index) return;
+
+        ServerData.Index++;
+
+        // TODO reward the item here
+        // if items can be received while in an invalid state for actually handling them, they can be placed in a local
+        // queue/collection to be handled later
+    }
+
+    /// <summary>
+    /// something went wrong with our socket connection
+    /// </summary>
+    /// <param name="e">thrown exception from our socket</param>
+    /// <param name="message">message received from the server</param>
+    private void OnSessionErrorReceived(Exception e, string message)
+    {
+        Plugin.BepinLogger.LogError(e);
+        ArchipelagoConsole.LogMessage(message);
+    }
+
+    /// <summary>
+    /// something went wrong closing our connection. disconnect and clean up
+    /// </summary>
+    /// <param name="reason"></param>
+    private void OnSessionSocketClosed(string reason)
+    {
+        Plugin.BepinLogger.LogError($"Connection to Archipelago lost: {reason}");
+        Disconnect();
+    }
+}
---
+++ templates/ArchipelagoBepin6Template.Net.CoreCLR/Plugin.cs
@@ -0,0 +1,75 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using ArchipelagoBepin6Template.Net.CoreCLR.Archipelago;
+using ArchipelagoBepin6Template.Net.CoreCLR.Utils;
+using BepInEx.NET.Common;
+using UnityEngine;
+
+namespace ArchipelagoBepin6Template.Net.CoreCLR;
+
+[BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+public class Plugin : BasePlugin
+{
+    public const string PluginGUID = "com.yourName.projectName";
+    public const string PluginName = "ArchipelagoBepin6Template.Net.CoreCLR";
+    public const string PluginVersion = "1.0.0";
+
+    public const string ModDisplayInfo = $"{PluginName} v{PluginVersion}";
+    private const string APDisplayInfo = $"Archipelago v{ArchipelagoClient.APVersion}";
+    public static ManualLogSource BepinLogger;
+    public static ArchipelagoClient ArchipelagoClient;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        BepinLogger = Log;
+        ArchipelagoClient = new ArchipelagoClient();
+        ArchipelagoConsole.Awake();
+
+        ArchipelagoConsole.LogMessage($"{ModDisplayInfo} loaded!");
+    }
+
+    private void OnGUI()
+    {
+        // show the mod is currently loaded in the corner
+        GUI.Label(new Rect(16, 16, 300, 20), ModDisplayInfo);
+        ArchipelagoConsole.OnGUI();
+
+        string statusMessage;
+        // show the Archipelago Version and whether we're connected or not
+        if (ArchipelagoClient.Authenticated)
+        {
+            // if your game doesn't usually show the cursor this line may be necessary
+            // Cursor.visible = false;
+
+            statusMessage = " Status: Connected";
+            GUI.Label(new Rect(16, 50, 300, 20), APDisplayInfo + statusMessage);
+        }
+        else
+        {
+            // if your game doesn't usually show the cursor this line may be necessary
+            // Cursor.visible = true;
+
+            statusMessage = " Status: Disconnected";
+            GUI.Label(new Rect(16, 50, 300, 20), APDisplayInfo + statusMessage);
+            GUI.Label(new Rect(16, 70, 150, 20), "Host: ");
+            GUI.Label(new Rect(16, 90, 150, 20), "Player Name: ");
+            GUI.Label(new Rect(16, 110, 150, 20), "Password: ");
+
+            ArchipelagoClient.ServerData.Uri = GUI.TextField(new Rect(150, 70, 150, 20),
+                ArchipelagoClient.ServerData.Uri);
+            ArchipelagoClient.ServerData.SlotName = GUI.TextField(new Rect(150, 90, 150, 20),
+                ArchipelagoClient.ServerData.SlotName);
+            ArchipelagoClient.ServerData.Password = GUI.TextField(new Rect(150, 110, 150, 20),
+                ArchipelagoClient.ServerData.Password);
+
+            // requires that the player at least puts *something* in the slot name
+            if (GUI.Button(new Rect(16, 130, 100, 20), "Connect") &&
+                !ArchipelagoClient.ServerData.SlotName.IsNullOrWhiteSpace())
+            {
+                ArchipelagoClient.Connect();
+            }
+        }
+        // this is a good place to create and add a bunch of debug buttons
+    }
+}
